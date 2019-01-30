c98fd402f8ae9e53ca885626db12983df2416dff
hasan <45696350+hasanhammad@users.noreply.github.com>
2019-01-30 16:05:27 +0330

    adding animation

--- main.cpp
+++ main.cpp
@@ -13,6 +13,26 @@
 #include"Basu_SVG_Language.h"
 
 
+using namespace std ;
+string cut (string line )
+
+{  string str;
+int j[2]  ;
+int k = 0 ;
+
+    for(int  i = 0;i < line.length();i++)
+{
+    if(line[i] == '"')
+    {
+        j[k++]=i;
+        str = line.substr (j[0]+1 , j[1]-j[0]-1  );
+
+    }
+
+}
+
+return str ;
+}
 
 double stod ( std::string Text )//Text not by const reference so that the function can be used with a
 {                               //character array as argument
@@ -41,7 +61,7 @@ std::stringstream ss(Text);
 {
     //if (name == "hasan")
 
-
+std::cout<<"haha"<<std::endl;
          return new attribute(name) ;
 
 
@@ -50,7 +70,23 @@ std::stringstream ss(Text);
 
     return NULL;
 }
-using namespace std ;
+
+ animation* create_animate( std::string name , std::string type ,std::string name1 )
+{
+    //if (name == "hasan")
+
+std::cout<<"haha1"<<std::endl;
+         return new animation(name , type ,name1) ;
+
+
+
+    //if (name == "Enemy") return new Enemy();
+
+    return NULL;
+}
+
+
+
 
 
 
@@ -60,41 +96,61 @@ using namespace std ;
 int main()
 {
 
-
-//shape prect = new rectangle() ;
-//prect.tag_print();
+animate n ;
 
 
-    vector <string > objects = {"rectangle","circle"};
+
+
+    string objects[] = {"rectangle","circle","ellipse","line","polygon","polyline","path","text" };
      vector <string > name1 ;
      std::vector <attribute*> type;
+     std::vector <animation*> animate;
 
-      vector <string >details = {"x","y","fill","width","height","r"};
+
     shape shape;
-     string arr[4];
+
+     string arr[5];
     string line ;
     while (getline(cin , line ))
     {
+
     int i = 0;
     stringstream ssin(line);
-    while (ssin.good() && i < 4)
+    while (ssin.good() && i < 5)
         {
         ssin >> arr[i];
         ++i;
         }
 
-        // set statement
 
+
+//print
         if (arr[0]=="print")
         {
+            for (animation* i : animate )
+            n.final_vector_animate(i);
+
+             for (attribute* i : type )
+            {
+                n.get_size(i);
+            }
             for (attribute* i : type )
+
             shape.final_vector(i);
 
+
+             for (animation* i : animate )
+
+            shape.final_vector_animate(i);
+            cout<<animate.size()<<"  size"<<endl;
+
             shape.tosvg ();
+
+
             shape.end_print();
         }
 
-
+// set statement
 
         if (arr[0] == "set" && arr[1]=="width")
         {
@@ -108,53 +164,132 @@ int main()
             shape.height_print(stod(arr[2])) ;
 
         }
-        // set statement end //
+// set statement end //
+
         if (arr[0] == "create"  )
         {cout<<"good"<<endl;
 
 
 
-            sort(objects.begin(),objects.end());
-            bool fn = binary_search(objects.begin(),objects.end(),arr[1]);
-            if (fn == true )
+            //sort(objects.begin(),objects.end());
+            //bool fn = binary_search(objects.begin(),objects.end(),arr[1]);
+            for (string i : objects )
+            if (i.find(arr[1], 0) != std::string::npos)
             {
 
-                cout<<"good1"<<endl;
+
 
 
                 if (arr[1]=="rectangle")
                 {
-                    type.push_back(create (arr[2])) ;
-
+                    attribute* a = create (arr[2]) ;
+                    type.push_back(a) ;
+                    cout<<"good1"<<endl;
                     for (attribute* i : type )
                         if(i->name == (arr[2]))
                     i->type ="rectangle" ;
 
 
-
-
                 }
 
-                name1.push_back(arr[2]);
-               if (arr[1]=="circle")
+
+               else if (arr[1]=="circle")
                {
-                   type.push_back(create (arr[2])) ;
+                    attribute* a = create (arr[2]) ;
+                    type.push_back(a) ;
+
                    for (attribute* i : type )
                         if(i->name == (arr[2]))
                     i->type ="circle" ;
 
                }
+               else if (arr[1]=="ellipse")
+                {
+                     attribute* a = create (arr[2]) ;
+                    type.push_back(a) ;
 
+                    for (attribute* i : type )
+                        if(i->name == (arr[2]))
+                    i->type ="ellipse" ;
 
+                }
+                else if (arr[1]=="line")
+                {
+                     attribute* a = create (arr[2]) ;
+                    type.push_back(a) ;
 
+                    for (attribute* i : type )
+                        if(i->name == (arr[2]))
+                    i->type ="line" ;
 
+                }
+                else if (arr[1]=="polygon")
+                {
+                     attribute* a = create (arr[2]) ;
+                    type.push_back(a) ;
 
+                    for (attribute* i : type )
+                        if(i->name == (arr[2]))
+                    i->type ="polygon" ;
 
+                }
+                else if (arr[1]=="polyline")
+                {
+                     attribute* a = create (arr[2]) ;
+                    type.push_back(a) ;
+
+                    for (attribute* i : type )
+                        if(i->name == (arr[2]))
+                    i->type ="polyline" ;
+
+                }
+                 else if (arr[1]=="path")
+                {
+                     attribute* a = create (arr[2]) ;
+                    type.push_back(a) ;
+
+                    for (attribute* i : type )
+                        if(i->name == (arr[2]))
+                    i->type ="path" ;
+
+                }
+                 else if (arr[1]=="text")
+                {
+                     attribute* a = create (arr[2]) ;
+                    type.push_back(a) ;
+
+                    for (attribute* i : type )
+                        if(i->name == (arr[2]))
+                    i->type ="text" ;
+
+                }
 
         }
 
         }
+//end create
 
+ if (arr[0]=="animation")
+                      {
+                          cout<<"ok"<<endl;
+                          for (attribute* i : type )
+                          if(i->name == (arr[1]))
+                          {
+                               animation* n = create_animate(i->name , i->type , arr[2]) ;
+                               animate.push_back(n) ;
+
+                              i->animate = true ;
+                              cout<<"true"<<endl;
+
+
+                          }
+
+
+
+
+                      }
+
+// set
 
 
 
@@ -202,6 +337,52 @@ int main()
                       i->fill_color=arr[3];
 
                   }
+                   if (arr[2]=="stroke"&&arr[3]!="width")
+                  {
+
+                      for (attribute* i : type )
+                        if(i->name == (arr[1]))
+                        {
+                            i->stroke=arr[3];
+                     cout<<i->stroke<<endl;
+                        }
+
+
+                  }
+                   if (arr[2]=="stroke" && arr[3]=="width")
+                  {
+                      cout<<"yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"<<endl;
+
+                      for (attribute* i : type )
+                        if(i->name == (arr[1]))
+                     i->stroke_width=stod(arr[4]);
+
+                  }
+                   if (arr[2]=="opacity")
+                  {
+
+                     for (attribute* i : type )
+                        if(i->name == (arr[1]))
+                      i->opacity=stod(arr[3]);
+
+                  }
+                   if (arr[2]=="rx")
+                  {
+
+                     for (attribute* i : type )
+                        if(i->name == (arr[1]))
+                      i->rx=stod(arr[3]);
+
+                  }
+                   if (arr[2]=="ry")
+                  {
+
+                     for (attribute* i : type )
+                        if(i->name == (arr[1]))
+                      i->ry=stod(arr[3]);
+
+                  }
+
                    if (arr[2]=="cx")
                   {
 
@@ -226,6 +407,140 @@ int main()
                       i->r=stod(arr[3]);
 
                   }
+                  if (arr[2]=="x1")
+                  {
+                      cout<<"x"<<endl;
+                      for (attribute* i : type )
+                        if(i->name == (arr[1]))
+                      i->x1=stod(arr[3]);
+
+                  }
+                   if (arr[2]=="x2")
+                  {
+                      cout<<"x"<<endl;
+                      for (attribute* i : type )
+                        if(i->name == (arr[1]))
+                      i->x2=stod(arr[3]);
+
+                  }
+                   if (arr[2]=="y1")
+                  {
+                      cout<<"x"<<endl;
+                      for (attribute* i : type )
+                        if(i->name == (arr[1]))
+                      i->y1=stod(arr[3]);
+
+                  }
+                   if (arr[2]=="y2")
+                  {
+                      cout<<"x"<<endl;
+                      for (attribute* i : type )
+                        if(i->name == (arr[1]))
+                      i->y2=stod(arr[3]);
+
+                  }
+                   if (arr[2]=="points")
+                  {
+                      line = cut(line);
+                      for (attribute* i : type )
+                        if(i->name == (arr[1]))
+                            i->points=line;
+
+
+
+                  }
+                  if (arr[2]=="d")
+                  {
+                      line = cut(line);
+                      for (attribute* i : type )
+                        if(i->name == (arr[1]))
+                            i->d=line;
+
+
+
+                  }
+                   if (arr[2]=="font"&&arr[3]=="size")
+                  {
+
+                      for (attribute* i : type )
+                        if(i->name == (arr[1]))
+                        {
+                            i->font_size=stod(arr[4]);
+                     cout<<i->font_size<<endl;
+                        }
+
+
+                  }
+                    if (arr[2]=="font"&&arr[3]=="family")
+                  {
+
+                      for (attribute* i : type )
+                        if(i->name == (arr[1]))
+                        {
+                            i->font_family=arr[4];
+                     cout<<i->font_family<<endl;
+                        }
+
+
+                  }
+                  if (arr[2]=="txt")
+                  {
+                          line = cut(line);
+                      for (attribute* i : type )
+                        if(i->name == (arr[1]))
+                        {
+                            i->txt=line;
+                     cout<<i->txt<<endl;
+                        }
+
+
+                  }
+
+                   if (arr[3]=="dur")
+                              {
+                                  for (animation* i : animate )
+                                    if(i->name == (arr[1] ) && i->name1 == (arr[2] ))
+                                    {
+                                        i->dur=arr[4];
+                                 cout<<i->dur<<endl;
+                                    }
+                              }
+                              if (arr[3]=="attributeName")
+                              {
+                                  for (animation* i : animate )
+                                    if(i->name == (arr[1] ) && i->name1 == (arr[2] ))
+                                    {
+                                        i->attributeName=arr[4];
+                                 cout<<i->attributeName<<endl;
+                                    }
+                              }
+
+                               if (arr[3]=="from")
+                              {
+                                  for (animation* i : animate )
+                                    if(i->name == (arr[1] ) && i->name1 == (arr[2] ))
+                                    {
+                                        i->from=stod(arr[4]);
+                                 cout<<i->from<<endl;
+                                    }
+                              }
+                               if (arr[3]=="to")
+                              {
+                                  for (animation* i : animate )
+                                    if(i->name == (arr[1] ) && i->name1 == (arr[2] ))
+                                    {
+                                        i->to=stod(arr[4]);
+                                 cout<<i->to<<endl;
+                                    }
+                              }
+                              if (arr[3]=="repeatCount")
+                              {
+                                  for (animation* i : animate )
+                                    if(i->name == (arr[1] ) && i->name1 == (arr[2] ))
+                                    {
+                                        i->repeatCount=arr[4];
+                                 cout<<i->repeatCount<<endl;
+                                    }
 
 
 
@@ -234,21 +549,13 @@ int main()
 
 
 
-            }
+                               }
 
 
 
 
 
 
-
-
-      for(unsigned int i = 0; i < type.size(); ++i) {
-
-                      cout<<"new idea "<<type[i]->name<<type[i]->type<<type[i]->x<<endl;
-
-
-                      }
 
 
        }
@@ -256,7 +563,7 @@ int main()
 
 
 
-
+    }
 
 
 
